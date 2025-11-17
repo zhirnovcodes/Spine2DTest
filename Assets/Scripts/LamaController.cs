208022d5bb5dc9bf91c1d01e50392625e635b87e
Ivan Zhirnov <i.zhirnov@datasakura.com>
2025-11-17 11:49:16 +0700

    Fixing button input

--- Assets/Scripts/LamaController.cs
+++ Assets/Scripts/LamaController.cs
@@ -5,14 +5,6 @@ public class LamaController : MonoBehaviour
 {
     public Animator Animator;
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Jump();
-        }
-    }
-
     public void Jump()
     {
         Animator.SetTrigger("Jump");
